9979b9b4feb32a416c13155ac26e981e6639417e
Anton Tsvetkov <anton_tsvetkov@epam.com>
2020-12-08 21:54:34 +0300

    complete sort by lines length task

---
+++ src/LinesSorter.java
@@ -0,0 +1,55 @@
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+public class LinesSorter {
+
+    public static void sortByLinesLength(String path, boolean append) {   // append for FileWriter
+        List<String> strings = new ArrayList<>();
+
+        //--------------------------------------------------READ--------------------------------------------------//
+        try (FileReader reader = new FileReader(path)) {
+            int c = 0;
+            String line;
+            while (c != -1) {
+                line = "";
+                do {
+                    c = reader.read();
+                    if (c != -1) {
+                        line = line.concat(String.valueOf((char) c));
+                    }
+                } while ((char) c != '\n' && c != -1);
+                strings.add(line);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        //--------------------------------------------WRITE and SORT--------------------------------------------//
+        try (FileWriter writer = new FileWriter(path, append)) {
+
+            Comparator<String> comp = new Comparator<String>() {
+                public int compare(String one, String two) {
+                    return one.length() - two.length();
+                }
+            };
+
+            strings.sort(comp);
+            for (String string : strings) {
+                writer.write(string);
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
+
+
+
+
+
+
