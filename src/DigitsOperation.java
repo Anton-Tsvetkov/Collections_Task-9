01c2ac750d01f3f1b7b4ea210337902a08a6a7ec
Anton Tsvetkov <anton_tsvetkov@epam.com>
2020-12-09 13:32:00 +0300

    rename class

--- src/DigitsOperation.java
+++ src/DigitsOperation.java
@@ -1,26 +1,20 @@
 import java.util.Stack;
 
-public class DigitsReverse {
+public class DigitsOperation {
 
     public static void digitsReverse(int number) {
 
-        String numb = String.valueOf(number);
+        String numbAsString = String.valueOf(number);
 
         Stack<Character> digits = new Stack<>();
         String reverse = "";
 
-        for(int i = 0; i < numb.length(); i++){
-            digits.push(numb.charAt(i));
+        for(int i = 0; i < numbAsString.length(); i++){
+            digits.push(numbAsString.charAt(i));
         }
         while (!digits.empty()) {
             reverse = reverse.concat(String.valueOf(digits.pop()));
         }
         System.out.println(reverse);
     }
-
-    public static void main(String[] args) {
-        digitsReverse(12345);
-
-    }
-
 }
