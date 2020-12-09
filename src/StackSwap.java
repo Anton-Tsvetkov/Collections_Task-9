c1096ec5d297c409e163e08a8544e558249e683c
Anton Tsvetkov <anton_tsvetkov@epam.com>
2020-12-09 10:31:25 +0300

    complete swap stack task

--- src/StackSwap.java
+++ src/StackSwap.java
@@ -2,17 +2,10 @@ import java.util.Stack;
 
 public class StackSwap {
 
-    public static void stackSwap(Stack<Integer> one, Stack<Integer> two) {
+    public static void stackSwap(Stack<Object> one, Stack<Object> two) {
         Object copyOne = one.clone();
         Object copyTwo = two.clone();
-        one = (Stack<Integer>) copyTwo;
-        two = (Stack<Integer>) copyOne;
+        one = (Stack<Object>) copyTwo;
+        two = (Stack<Object>) copyOne;
     }
-
-
-    public static void main(String[] args) {
-
-
-    }
-
 }
