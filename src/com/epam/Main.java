d71ed5466e601106c1ef1164df5b29d3d21a491b
Anton Tsvetkov <anton_tsvetkov@epam.com>
2020-12-09 16:54:18 +0300

    add com.epam package, main method and rename class

--- src/com/epam/Main.java
+++ src/com/epam/Main.java
@@ -1,7 +1,9 @@
+package com.epam;
+
 import java.util.HashSet;
 import java.util.Set;
 
-public class SetsIntersectionAndUnion {
+public class Main {
 
     public static Set<Integer> intersectionSets(Set<Integer> one, Set<Integer> two){
         Set<Integer> intersectionSet = new HashSet<>();
@@ -18,6 +20,23 @@ public class SetsIntersectionAndUnion {
     }
 
 
+    public static void main(String[] args) {
+        Set<Integer> one = new HashSet<>();
+        one.add(1);
+        one.add(2);
+        one.add(3);
+        one.add(4);
+        one.add(5);
 
+        Set<Integer> two = new HashSet<>();
+        two.add(6);
+        two.add(3);
+        two.add(7);
+        two.add(9);
+        two.add(4);
+
+        System.out.println("Intersection: " + intersectionSets(one, two));
+        System.out.println("Union: " + unionSets(one, two));
+    }
 
 }
