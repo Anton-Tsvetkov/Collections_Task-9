f0fa916f3b1cc990e055d6f98d669b08233145b3
Anton Tsvetkov <anton_tsvetkov@epam.com>
2020-12-09 17:42:34 +0300

    done brackets placement check

--- src/com/epam/Main.java
+++ src/com/epam/Main.java
@@ -1,29 +1,44 @@
 package com.epam;
 
-
-// 8. Задана строка, состоящая из символов «(», «)», «[», «]», «{», «}».
-//    Проверить правильность расстановки скобок.
-//    Использовать стек.
+import java.util.Stack;
 
 public class Main {
-    public static boolean isBracketsOrderCorrect(String string){
-        boolean bracketsOrderCorrect = false;
-
-
-
-        return bracketsOrderCorrect;
+    public static boolean isBracketsOrderCorrect(String string) {
+        Stack<Character> bracketsStack = new Stack<>();
+        for (int i = 0; i < string.length(); i++) {
+            if (string.charAt(i) == '('
+                    || string.charAt(i) == '['
+                    || string.charAt(i) == '{') {
+                bracketsStack.push(string.charAt(i));
+            } else if (string.charAt(i) == ')'
+                    || string.charAt(i) == ']'
+                    || string.charAt(i) == '}') {
+                if (!bracketsStack.empty()) {
+                    char pop = bracketsStack.pop();
+                    if (pop == '(' && string.charAt(i) != ')'
+                            || pop == '[' && string.charAt(i) != ']'
+                            || pop == '{' && string.charAt(i) != '}') {
+                        return false;
+                    }
+                } else {
+                    return false;
+                }
+            }
+        }
+        return true;
     }
 
     public static void main(String[] args) {
 
-        String strCorrect = "()[]{}";
+        String strCorrect1 = "()[]{}";
+        String strCorrect2 = "()[{}]";
         String strWrong1 = ")([[{}]";
         String strWrong2 = "()[{]}";
 
-        System.out.println(isBracketsOrderCorrect(strCorrect));
+        System.out.println(isBracketsOrderCorrect(strCorrect1));
+        System.out.println(isBracketsOrderCorrect(strCorrect2));
         System.out.println(isBracketsOrderCorrect(strWrong1));
         System.out.println(isBracketsOrderCorrect(strWrong2));
-
 
 
     }
