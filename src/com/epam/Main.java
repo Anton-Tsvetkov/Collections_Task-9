b6416765405024d79e006a16ba57c674e85ac548
Anton Tsvetkov <anton_tsvetkov@epam.com>
2020-12-09 17:47:18 +0300

    add base of project

---
+++ src/com/epam/Main.java
@@ -0,0 +1,13 @@
+package com.epam;
+
+import java.util.HashSet;
+
+public class Main {
+
+    public static HashSet<String> searchDifferentWords(){
+        HashSet<String> differentWordsSet = new HashSet<>();
+
+
+        return differentWordsSet;
+    }
+}
